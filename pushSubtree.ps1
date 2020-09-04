ea7fff27c6e247cdc53e8bfda50129793f0e6f6f
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-05 01:19:09 +0530

    Adding push command for all subtrees used in this repo

---
+++ pushSubtree.ps1
@@ -0,0 +1,4 @@
+git subtree push --prefix=property https://github.com/prashanthsarma/property-portal-property.git master
+git subtree push --prefix=client https://github.com/prashanthsarma/property-portal-client.git master
+git subtree push --prefix=auth https://github.com/prashanthsarma/property-portal-auth.git master
+git subtree push --prefix=common https://github.com/prashanthsarma/property-portal-common.git master
