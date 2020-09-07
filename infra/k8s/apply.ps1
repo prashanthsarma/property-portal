8082ba46b4f9ab8a1246c31aa1428ba479ae5cf7
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-07 10:06:13 +0530

    Adding deployment changes for property-srv

--- infra/k8s/apply.ps1
+++ infra/k8s/apply.ps1
@@ -1,5 +1,7 @@
+kubectl apply -f ingress-srv.yaml
+
 kubectl apply -f auth-mongo-depl.yaml
 kubectl apply -f auth-depl.yaml
+kubectl apply -f property-mongo-depl.yaml
+kubectl apply -f property-depl.yaml
 kubectl apply -f client-depl.yaml
-
-kubectl apply -f ingress-srv.yaml
