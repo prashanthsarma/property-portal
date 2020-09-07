3502d4f7f7d67d26f918b6d477328f4bc29a91e8
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-07 17:46:49 +0530

    adding rollout restart command

--- infra/k8s/apply.ps1
+++ infra/k8s/apply.ps1
@@ -5,3 +5,7 @@ kubectl apply -f auth-depl.yaml
 kubectl apply -f property-mongo-depl.yaml
 kubectl apply -f property-depl.yaml
 kubectl apply -f client-depl.yaml
+
+kubectl rollout restart deployment auth-depl
+kubectl rollout restart deployment property-depl
+kubectl rollout restart deployment client-depl
