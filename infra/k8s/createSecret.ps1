d8cda12b44fde5d2af245847281adad8c90721a1
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-09 09:48:45 +0530

    Changes done during deployment

--- infra/k8s/createSecret.ps1
+++ infra/k8s/createSecret.ps1
@@ -1,7 +1,8 @@
 # The values of all secrets are  not available here...
-# This file is just a reminder to creat the required secrets
+# This file is just a reminder to create the required secrets
 
 # openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=ramproper.com /O=ramproper.com"
 # kubectl create secret tls tls-secret --key domain.com.key --cert domain.com.cert
 
 # kubectl create secret generic jwt-secret --from-literal=JWT_KEY=xxx
+# kubectl create secret generic aws-secret --from-literal=AWS_SECRET=xxx
